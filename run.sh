1dae808bfe4f8fe4fab6ec147d2710ed66e2962e
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-02-23 19:25:57 +0100

    update

--- run.sh
+++ run.sh
@@ -21,6 +21,7 @@ sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /root/.bash_profile
 sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /home/ec2-user/.bash_profile
 source /home/ec2-user/.bash_profile
 /usr/local/bin/python3.9 -m pip install --upgrade pip
+cd /home/ec2-user || exit
 git clone https://github.com/sinanartun/binance_4.git
 sudo chown -R ec2-user:ec2-user /home/ec2-user/binance_4
 sudo chmod 2775 /home/ec2-user/binance_4 && find /home/ec2-user/binance_4 -type d -exec sudo chmod 2775 {} \;
