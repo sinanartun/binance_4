900e5fad91df9f21eec97e012ad50192b5af0f50
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-02-23 19:00:27 +0100

    update

--- run.sh
+++ run.sh
@@ -19,9 +19,11 @@ cd Python-3.9.16 || exit
 sudo make altinstall
 sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /root/.bash_profile
 sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /home/ec2-user/.bash_profile
-source ~/.bash_profile
+source /home/ec2-user/.bash_profile
 /usr/local/bin/python3.9 -m pip install --upgrade pip
 git clone https://github.com/sinanartun/binance_4.git
+sudo chown -R ec2-user:ec2-user /home/ec2-user/binance_4
+sudo chmod 2775 /home/ec2-user/binance_4 && find /home/ec2-user/binance_4 -type d -exec sudo chmod 2775 {} \;
 cd binance_4 || exit
 python3.9 -m venv venv
 pip3.9 install -r requirements.txt
