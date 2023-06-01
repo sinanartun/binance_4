9e67b4aaf46a8eb7cf262c40a134bbe52d467e25
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-06-01 19:00:47 +0200

    update

--- token.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ROLE_NAME=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/) && \
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600") && \
-curl -H "X-aws-ec2-metadata-token: $TOKEN" -v "http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE_NAME"
-
-
-
