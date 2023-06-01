bbcf05d91913881eabb01259aa7d7f4854cdab8c
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-06-01 19:19:40 +0200

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user || exit
-sudo yum upgrade -y
-sudo yum -y groupinstall "Development Tools"
-sudo yum -y install openssl-devel bzip2-devel libffi-devel python3-pip
+sudo dnf upgrade -y
+sudo dnf -assumeyes groupinstall "Development Tools"
+sudo dnf install --assumeyes python3-pip libffi-devel openssl-devel bzip2-devel
 git clone https://github.com/sinanartun/binance_5.git
 sudo chown -R ec2-user:ec2-user /home/ec2-user/binance_5
 sudo chmod 2775 /home/ec2-user/binance_5 && find /home/ec2-user/binance_5 -type d -exec sudo chmod 2775 {} \;
