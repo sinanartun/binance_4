9136decc2a7afa974b378ff19f4c8ee4e186fd9c
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-06-09 13:03:51 +0200

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 cd /home/ec2-user || exit
-sudo dnf upgrade -y
-sudo dnf -assumeyes groupinstall "Development Tools"
-sudo dnf install --assumeyes python3-pip libffi-devel openssl-devel bzip2-devel
-git clone https://github.com/sinanartun/binance_5.git
-sudo chown -R ec2-user:ec2-user /home/ec2-user/binance_5
-sudo chmod 2775 /home/ec2-user/binance_5 && find /home/ec2-user/binance_5 -type d -exec sudo chmod 2775 {} \;
-cd binance_5 || exit
-python3 -m venv venv
-pip3 install -r requirements.txt
-python3 main.py
+sudo yum upgrade -y
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install openssl-devel bzip2-devel libffi-devel python3-pip
+curl -o ./Python-3.9.16.tgz https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
+tar xvf Python-3.9.16.tgz
+cd Python-3.9.16 || exit
+./configure --enable-optimizations
+sudo make altinstall
+sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /root/.bash_profile
+sed -i 's/^PATH=.*/&:\/usr\/local\/bin/' /home/ec2-user/.bash_profile
+source ~/.bash_profile
+/usr/local/bin/python3.9 -m pip install --upgrade pip
+cd /home/ec2-user || exit
+git clone https://github.com/sinanartun/binance_4.git
+sudo chown -R ec2-user:ec2-user /home/ec2-user/binance_4
+sudo chmod 2775 /home/ec2-user/binance_4 && find /home/ec2-user/binance_4 -type d -exec sudo chmod 2775 {} \;
+cd binance_4 || exit
+python3.9 -m venv venv
+pip3.9 install -r requirements.txt
+sudo tee /etc/systemd/system/binance.service << EOF
+[Unit]
+Description=Binance Service
+
+[Service]
+User=root
+ExecStart=/usr/local/bin/python3.9 /home/ec2-user/binance_4/main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable binance
+sudo systemctl start binance
